c79cc7b60aa547e1b249f4cb61d01bbf4f06d831
jredondoh <jredondoh@gmail.com>
2021-11-17 22:40:26 +0100

    1st smart-contract draft

--- contracts/nfts-for-friends.sol
+++ contracts/nfts-for-friends.sol
@@ -1,20 +1,30 @@
-pragma solidity ^0.8;
+pragma solidity 0.8.2;
 
 contract NFTsForFriends {
-    modifier isRegistered(){
+    mapping (address => bool) private _registeredAddresses;
+    mapping (uint256 => mapping (address => uint256)) private _NFTShares;
+
+    modifier isRegistered(address _address){
         // checks that the sender is registered
+        require(_registeredAddresses[_address]);
+        _;
     }
 
     modifier isNFTPublished(uint256 _NFTId){
         // checks that the NFT is published
+        require(true);
+        _;
     }
 
     modifier isNFTNotYetAcquired(uint256 _NFTId){
         // checks that the NFT has not been acquired yet
+        require(true);
+        _;
     }
 
     function registerIn() public {
         // registers the sender in the marketplace
+        _registeredAddresses[msg.sender] = true;
     }
 
     function publishNFT(uint256 hashNFT, uint256 price)
